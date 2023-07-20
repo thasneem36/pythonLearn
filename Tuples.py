6cf9586ef56be2b4b9b9a77f90096284f28fb92c
thasneem <thasneem9mt@gmail.com>
2023-07-20 19:58:30 +0530

    add python master

---
+++ Tuples.py
@@ -0,0 +1,11 @@
+tup = (1,2,3,'a','b','c')
+tup1 = (1,2,1,3)
+
+'''for i in tup:
+    print(i)
+
+rep = ("hi")*3
+print(rep)
+
+'''
+print(max(tup1))
---
+++ list.py
@@ -0,0 +1,33 @@
+'''lst = [1,2,3,4,'a','b']
+
+lst2 = [0.1,0.2,0.3,0.4]
+
+puls = lst + lst2
+
+for i in lst:
+    print(i)
+
+a = ['hi'] * 5
+print(a)'''
+
+
+'''lst = [1,2,3]
+lst1 = ['a','b','c']
+
+print(min(lst)," ",max(lst))
+print(min(lst1)," ",max(lst1))
+
+lst.append(4)
+lst.append(5)
+
+#a=lst.index(2)
+#lst.remove(3)
+a=lst.pop(2)'''
+
+l=[1,2,3,4,5,6]
+
+#a=l.reverse()
+a=l.sort()#reverse=True
+
+print(a)
+print(l)
