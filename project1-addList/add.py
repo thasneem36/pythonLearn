4d866a5907afe8935be5a8ddfd7dc0bf7c93a95d
thasneem <thasneem9mt@gmail.com>
2023-08-11 23:01:18 +0530

    Add python

--- project1-addList/add.py
+++ project1-addList/add.py
@@ -2,8 +2,11 @@
 
 store_num = []
 store_str = []
+store_sp = []
 
+#datas
 num_data = ["1","2","3","4","5","6","7","8","9","0"]
+spe_ch = ["!","@","#","$","%","^","&","*","(",")","{","}","]","[",":",";","'","<",",",".",">","?","/","+","-","*","/"]
 
 #select option 
 print("Enter the multipule data < 1 >\nEnter the one data < 2 >")
@@ -19,6 +22,10 @@ if input_option == "1":
         if i in num_data:
             con_int=int(i)
             store_num.append(con_int)
+
+        elif i in spe_ch:
+            con_str_sp=str(i)
+            store_sp.append(con_str_sp)
     
         else:
             con_str=str(i)
@@ -36,6 +43,10 @@ else:
             if i in num_data:
                 con_int=int(i)
                 store_num.append(con_int)
+
+            elif i in spe_ch:
+                con_str_sp=str(i)
+                store_sp.append(con_str_sp)
     
             else:
                 con_str=str(i)
@@ -53,6 +64,8 @@ print()
 print("the filter of your word as a string :) ",store_str)
 print()
 print("the filter of your word as a integer or Number :) ",store_num)
+print()
+print("the filter of your word as a spacial chacter :) ",store_sp)
 print()
         
         
