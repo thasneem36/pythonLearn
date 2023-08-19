353e14e0709b989b381460b890f6c273d481ff58
thasneem <thasneem9mt@gmail.com>
2023-08-19 12:53:41 +0530

    add python master

--- 2project-Guess_Num_user.py
+++ 2project-Guess_Num_user.py
@@ -4,6 +4,7 @@ def guess(x):
     
     random_number = random.randint(1,x)
     guess = 0
+    
     while guess != random_number:
         guess = int(input(f'guess a number between 1 and {x}: '))
         
